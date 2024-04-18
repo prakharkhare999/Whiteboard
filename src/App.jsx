a057990daece183ed96a5b72aa2452447eca4157
prakharkhare999 <prakhark113@gmail.com>
2024-04-18 20:50:03 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Tldraw } from 'tldraw'
+import '@tldraw/tldraw/tldraw.css'
+
+export default function App() {
+	return (
+		<div style={{ position: 'fixed', inset: 0 }}>
+         
+			<Tldraw />
+ 
+		</div>
+	)
+}
